f69982f4fb084cbea8ff72f0ab871c74d1a15a40
Pawel Mstowski <pawel.mstowski@gmail.com>
2017-02-17 14:15:41 +0100

    create signup page: add routing and page file

--- app/routes.js
+++ app/routes.js
@@ -5,5 +5,10 @@ module.exports = function(app, passport){
     res.render('../views/pages/index.ejs');
   });
 
+  //signup
+  app.get('/signup', function(req,res){
+    res.render('../views/pages/signup.ejs', {message: req.flash('signupMessage')});
+  })
+
   //Login
 };
